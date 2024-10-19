e7738795701eddfe05f57634df1ff71f7e7d1066
ammar bakheit <amar@altras.co.uk>
2024-10-19 15:34:35 +0400

    hotfix: yup validation

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -11,8 +11,10 @@ import { UserResponseType } from '../../core/types';
 const SignupSchema = Yup.object().shape({
   name: Yup.string().required('Name is required'),
   email: Yup.string().email('Invalid email').required('Email is required'),
-  password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
+  password: Yup.string().min(8, 'Password must be at least 8 characters').required('Password is required'),
 });
+
+
 const Signup = () => {
     const navigate = useNavigate();
 
